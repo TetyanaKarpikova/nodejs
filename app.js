932959f2167efa7f2ca2ec455b7b433e20c57d0b
Tetyana <tetyanajkn@gmail.com>
2021-02-19 00:22:10 +0200

    add second task

--- app.js
+++ app.js
@@ -1,53 +1,92 @@
 const fs = require('fs');
 const path = require('path');
 
-let normalPath18 = path.normalize(__dirname + '/1800');
+// let normalPath18 = path.normalize(__dirname + '/1800');
 
-let normalPath20 = path.normalize(__dirname + '/2000')
+// let normalPath20 = path.normalize(__dirname + '/2000')
 
 
-function moveFile(oldPath, newPath, genderStr) {
-    fs.readdir(oldPath, (err, files) => {
+// function moveFile(oldPath, newPath, genderStr) {
+//     fs.readdir(oldPath, (err, files) => {
 
+//         if (err) {
+//             console.log('________readdir___________________');
+//             console.log(err);
+//             console.log('__________________________________');
+
+//             return;
+//         }
+
+//         files.forEach(file => {
+//             let oldPathFile = path.normalize(oldPath + '/' + file);
+//             let newPathFile = path.normalize(newPath + '/' + file);
+//             fs.readFile(oldPathFile, (err, data) => {
+//                 if (err) {
+//                     console.log('___________readFile_________________');
+//                     console.log(err);
+//                     console.log('________________________________');
+
+//                     return;
+//                 }
+
+//                 let { gender } = JSON.parse(data);
+//                 if (gender === genderStr) {
+//                     fs.rename(oldPathFile, newPathFile, err => {
+//                         if (err) {
+//                             console.log('_____________rename_________________');
+//                             console.log(err);
+//                             console.log('________________________________');
+
+//                             return;
+
+//                         }
+//                     })
+
+//                 }
+//             })
+//         })
+//     });
+
+// }
+
+// moveFile(normalPath18, normalPath20, 'male');
+// moveFile(normalPath20, normalPath18, 'female');
+
+
+// вам потрбіно перемісти всі файлики з вкладених папок в іншу папку. Зробити всі файли на одному рівні вкладеності.
+const normalPath = path.normalize(__dirname + '/lesson1');
+
+function moveInLine(somePath) {
+    fs.readdir(somePath, (err, files) => {
         if (err) {
-            console.log('________readdir___________________');
+            console.log('____________readdir_____________');
             console.log(err);
-            console.log('__________________________________');
+            console.log('________________________________');
 
             return;
         }
 
-        files.forEach(file => {
-            let oldPathFile = path.normalize(oldPath + '/' + file);
-            let newPathFile = path.normalize(newPath + '/' + file);
-            fs.readFile(oldPathFile, (err, data) => {
-                if (err) {
-                    console.log('___________readFile_________________');
-                    console.log(err);
-                    console.log('________________________________');
-
-                    return;
-                }
-
-                let { gender } = JSON.parse(data);
-                if (gender === genderStr) {
-                    fs.rename(oldPathFile, newPathFile, err => {
+        files.forEach(item => {
+            let pathToFile = path.normalize(somePath + `/${item}`);
+            fs.stat(pathToFile, (err, stats) => {
+                if (stats.isDirectory()) {
+                    moveInLine(pathToFile);
+                   // console.log(pathToFile);
+                } else {
+                   
+                    fs.rename(pathToFile, path.normalize(normalPath + `/${item}`), err => {
                         if (err) {
-                            console.log('_____________rename_________________');
+                            console.log('____________readdir_____________');
                             console.log(err);
                             console.log('________________________________');
 
                             return;
-
                         }
                     })
-
                 }
             })
         })
-    });
-
+    })
 }
 
-moveFile(normalPath18, normalPath20, 'male');
-moveFile(normalPath20, normalPath18, 'female');
+moveInLine(normalPath);
