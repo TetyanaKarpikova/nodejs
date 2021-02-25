62a5a337aa3d9df993f06592cb4d3f7ee53d2a23
Tetyana <tetyanajkn@gmail.com>
2021-02-25 21:14:22 +0200

    test

--- middleware/user.middleware.js
+++ middleware/user.middleware.js
@@ -5,7 +5,7 @@ module.exports = {
     checkIsIdValid: (req, res, next) => {
         try {
             const userId = +req.params.userId;
-            
+
             if (userId < 0 || !Number.isInteger(userId) || Number.isNaN(userId)) {
                 throw new Error(errorMessages.NOT_VALID_ID['en']);
             }
@@ -18,17 +18,17 @@ module.exports = {
 
     checkIsUserValid: (req, res, next) => {
         try {
-            const { name, email, password, preferL = 'en' } = req.body;            
-            
-            if (!name || !email || !password){                
+            const { name, email, password, preferL = 'en' } = req.body;
+
+            if (!name || !email || !password) {
                 throw new Error(errorMessages.FIELD_IS_EMPTY[preferL]);
             }
 
-            if (name.length < 3){                
-                throw new Error(errorMessages.TOO_SHORT_NAME[preferL]);                
+            if (name.length < 3) {
+                throw new Error(errorMessages.TOO_SHORT_NAME[preferL]);
             }
 
-            if (password.length < 8){                
+            if (password.length < 8) {
                 throw new Error(errorMessages.TOO_SHORT_PASSWORD[preferL]);
             }
 
