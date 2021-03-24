ebb7f0b4ec90c12f0ea327c709f2b5ae5db5b002
Tetyana <tetyanajkn@gmail.com>
2021-03-24 22:49:45 +0200

    del token

--- Lesson12/cronjobs/delToken.js
+++ Lesson12/cronjobs/delToken.js
@@ -1,7 +1,11 @@
 const { o_authModel } = require('../dataBase/model');
 
 module.exports = async () => {
-    const tokenizers = await o_authModel.deleteMany({ createdAt: { $lte: new Date().toISOString() } });
+    const currentDay = new Date();
+    currentDay.setDate(currentDay.getDate() - 10);
+    const dateString = currentDay.toISOString();
+
+    const tokenizers = await o_authModel.deleteMany({ createdAt: { $lte: dateString } });
 
     // const tokenizers = await o_authModel.find({ createdAt: { $lte: new Date().toISOString() } });
     // const tokenizers = await o_authModel.find({ createdAt: { $lte: new Date('2021-03-06') } });
